a62fc6b72a894ce4eb8bb69058e0838cfc9f36c2
melon <meloncha0205@gmail.com>
2020-05-16 14:02:12 +0900

    board first

---
+++ README.md
@@ -0,0 +1,5 @@
+# board
+
+1. 게시판 - 프로젝트 생성 및 navbar
+    npm install --save ejs express mongoose body-parser method-override
+
---
+++ routes/home.js
@@ -0,0 +1,16 @@
+// routes/home.js
+
+const express = require('express');
+const router = express.Router();
+
+// Home
+
+router.get('/', (req, res) => {
+  res.render('home/welcome');
+});
+router.get('/about', (req, res) => {
+  res.render('home/about');
+});
+
+
+module.exports = router;
